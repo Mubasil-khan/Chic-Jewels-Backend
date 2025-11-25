2c3db0ef723e3de3f2553bdc2a43aed514be55d7
grocery-store-project <mubasil.emedstore@gmail.com>
2025-11-25 15:44:22 +0530

    fix issue

--- midalware/authuser.js
+++ midalware/authuser.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authUser = async (req, res, next) => {
 
-    const { token } = req.cookies;
+    const { UserToken } = req.cookies;
 
     if (!token) {
         return res.json({ success: false, message: 'not a token' })
